55a1bf46e351a29ff6d1dbc13650f6cd175ba8f7
Atharva Kishor Matale <160848806+AtharvaKishorMatale@users.noreply.github.com>
2025-07-29 10:27:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def students():
     return render_template("students.html", students=data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
